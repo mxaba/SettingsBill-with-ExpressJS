9cbd006e30c57f847e59974fcb3fcb2730d93df7
mxaba <samuel.mcebo@gmail.com>
2021-06-07 16:27:23 +0200

    Added view engine and handlebars

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 // eslint-disable-next-line import/no-unresolved
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Setting the public folder
 app.use(express.static('public'));
 
+// eslint-disable-next-line no-undef
+app.engine('handlebars', exphbs({ layoutsDir: './views/layouts' }));
+app.set('view engine', 'handlebars');
+
 app.listen(PORT, () => {
-  console.log('App is running on port: ', PORT);
+  console.log('App is running on port:', PORT);
 });
