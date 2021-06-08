ff33a9430d3b7c47c63ab510beb3cf178a162892
mxaba <samuel.mcebo@gmail.com>
2021-06-08 09:07:18 +0200

    added content and boddy pasrse but still not abe to get the beauty of the site

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // eslint-disable-next-line import/no-unresolved
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Setting the public folder
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // eslint-disable-next-line no-undef
 app.engine('handlebars', exphbs({ layoutsDir: './views/layouts' }));
